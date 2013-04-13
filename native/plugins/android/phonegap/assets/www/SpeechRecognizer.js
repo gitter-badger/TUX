d3fb2a9dcde4dc673bf909c6ecc0c432540d17fa
Andrea Cammarata <andrea.cammarata@sencha.com>
2013-04-13 15:54:32 +0200

    Added the javascript interface to the SpeechRecognizer PhoneGap plugin.

---
+++ native/plugins/android/phonegap/assets/www/SpeechRecognizer.js
@@ -0,0 +1,35 @@
+/**
+ * Cordova Speech Recognizer plugin for Android.
+ */
+cordova.define("cordova/plugin/speechrecognizer",
+  function(require, exports, module) {
+    var exec = require("cordova/exec");
+
+    var SpeechRecognizer = function() {};
+    
+    /**
+    * @param {Object} argument The json object which contains the arguments that should be used
+    * to initialize the speech recognition process.
+    * @param {Function} successCallback The callback which will be called when listDevices is successful
+    * @param {Function} failureCallback The callback which will be called when listDevices encouters an error
+    */
+    SpeechRecognizer.prototype.listen = function(argument, successCallback, failureCallback) {
+        exec(successCallback, failureCallback, 'SpeechRecognizer', 'listen', [argument]);
+    };
+
+    var speechRecognizer = new SpeechRecognizer();
+    module.exports = speechRecognizer;
+
+});
+
+
+if (!window.plugins) {
+    window.plugins = {};
+}
+if (!window.plugins.speechRecognizer) {
+    window.plugins.speechRecognizer = cordova.require("cordova/plugin/speechrecognizer");
+}
+
+if (!navigator.speechRecognizer) {
+    navigator.speechRecognizer = window.plugins.speechRecognizer;
+}
---
+++ native/plugins/android/phonegap/src/com/simacs/cordova/SpeechRecognizer.java
@@ -0,0 +1,93 @@
+package com.simacs.cordova;
+
+import java.util.ArrayList;
+
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.R;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.util.Log;
+import android.widget.EditText;
+
+public class SpeechRecognizer extends CordovaPlugin {
+	
+	public static final String ACTION_SPEECH = "listen";
+	
+	private static final String LOG_TAG = "SpeechRecognizerPlugin";
+	protected static final int RESULT_SPEECH = 1;
+	
+	private CallbackContext callbackContext;
+
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+
+		this.callbackContext = callbackContext;
+		
+		JSONObject params = args.getJSONObject(0);
+		
+	    if (ACTION_SPEECH.equals(action)) {
+	    	
+	    	String culture = (String)params.get("culture");
+	    	String prompt = (String)params.get("prompt");
+	    	
+	    	Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+	        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, culture);
+	        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, prompt);
+	        
+	    	try {
+	    		
+	    		cordova.setActivityResultCallback(this);
+	    		cordova.getActivity().startActivityForResult(intent, RESULT_SPEECH);
+	    		return true;
+	  
+	        } catch (ActivityNotFoundException a) {
+
+	        	callbackContext.error("Your device doesn't support Speech to Text.");
+	        	
+	        }
+
+		} 
+	    else {
+	    	
+	    	callbackContext.error("Invalid Action: " + action);
+	    	Log.d(LOG_TAG, "Invalid action : " + action);
+	    	
+		}
+	    
+		return false;
+	    
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		
+        super.onActivityResult(requestCode, resultCode, data);
+ 
+        switch (requestCode) {
+        
+	        case RESULT_SPEECH: {
+	        
+	            if (resultCode == Activity.RESULT_OK && null != data) {
+	 
+	                ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+	                this.callbackContext.success(text.get(0));
+	
+	            }
+	            
+	            break;
+	
+	        }
+ 
+        }
+        
+    }
+
+}
